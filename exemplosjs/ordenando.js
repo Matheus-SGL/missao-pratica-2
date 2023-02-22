2e8633933a83f6629a77514b1de20014a8ccd0fb
Matheus-SGL <matheus.roia@gmail.com>
2023-02-22 20:29:08 -0300

    att e add funcoes de ordenacao
    Adicionado descrição às funções;
    Reescrito função swap;
    Adicionado novas funções de ordenação.

--- exemplosjs/ordenando.js
+++ exemplosjs/ordenando.js
@@ -1,21 +1,75 @@
-let swap = (vetor, posicao, valor1, valor2) => vetor.splice(posicao, 2, valor1, valor2);
+// Função swap, com o objetivo de trocar os valores de duas posições de um vetor, 
+// tendo como parâmetros de entrada o vetor e as posições
+var swap = (vetor, posicao1, posicao2) => {
+    let aux = vetor[posicao1];
+    vetor[posicao1] = vetor[posicao2];
+    vetor[posicao2] = aux;
 
-let shuffle = (vetor, qtdTrocas) => {
+    return vetor;
+}
+
+
+// Função shuffle, com o objetivo de embaralhar os elementos de um vetor, tendo
+// como parâmetros de entrada o vetor e a quantidade de trocas
+var shuffle = (vetor, qtdTrocas) => {
     for(let x = 0; qtdTrocas > x; qtdTrocas--) {
         vetor.sort(() => Math.random() - 0.5);
     }
+    return vetor;
 }
 
-let bubble_sort = (vetor) => {
+
+//Função bubble_sort, para ordenar um vetor de inteiros com o algoritmo Bubble
+//Sort, tendo como parâmetro de entrada o vetor de inteiros
+var bubble_sort = (vetor) => {
     for (let x = 0; x < vetor.length; x++) {
         for (let y = 0; y < vetor.length; y++) {
             if (vetor[y] > vetor[y + 1]) {
-                let aux = vetor[y];
-                vetor[y] = vetor[y + 1];
-                vetor [y + 1] = aux;
+                swap(vetor, vetor[y], vetor[y+1]);
             }
         }
     }
     return vetor;
 }
 
+// Função selection_sort, para ordenar um vetor de inteiros utilizando o algoritmo
+// Selection Sort, tendo como parâmetro de entrada o vetor de valores inteiros
+var selection_sort = (vetor) => {
+    for(let x = 0; x < vetor.length; x++) {
+        let menor = x;
+        for (let y = x+1; y < vetor.length; y++) {
+            if (vetor[y] < vetor[menor]) { menor = y; }
+        }
+        if (menor != x) {
+            swap(vetor, vetor[x], vetor[menor]);
+        }
+    }
+    return vetor;
+}
+
+// Função quick_sort, para ordenar um vetor de inteiros com o algoritmo Quick Sort,
+// recursivo, tendo como parâmetros o vetor, posição inicial e posição final
+var quick_sort = (vetor, initPos, finalPos) => {
+    if (initPos < finalPos) {
+        let pivotPos = particionamento(vetor, initPos, finalPos);
+        quick_sort(vetor, initPos, pivotPos - 1);
+        quick_sort(vetor, pivotPos + 1, finalPos);
+    }
+    return vetor;
+}
+
+// Função particionamento, de apoio a quick_sort, tendo como parâmetros o vetor,
+// posição inicial e posição final.
+var particionamento = (vetor, initPos, finalPos) => {
+    let pivot = vetor[initPos];
+    let i = initPos;
+
+    for (let x = initPos + 1; x <= finalPos; x++) {
+        if (vetor[x] <= pivot) {
+            i++;
+            swap(vetor, i, x);
+        }
+    }
+    swap(vetor, initPos, i);
+    return i;
+}
